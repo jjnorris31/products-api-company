d6d1f8098acba323c4048fd2c452eba267f37944
jjnorris31 <jjnorris31@gmail.com>
2021-02-23 12:20:46 -0600

    fix: fixed babel missing import

--- src/app.js
+++ src/app.js
@@ -4,17 +4,19 @@ import pkg from '../package.json';
 import productsRoutes from './routes/products.routes';
 import authRoutes from './routes/auth.routes'
 import { createRoles } from './libs/initialSetup';
+import dotenv from 'dotenv';
 
 
 const app = express();
 createRoles();
+dotenv.config();
 
 // setting a const for app
 app.set('pkg', pkg);
 
 app.use(morgan('dev'));
 app.use(express.json())
-app.listen(3000);
+app.listen(process.env.PORT);
 
 app.get('/', (req, res) => {
 	res.json({
